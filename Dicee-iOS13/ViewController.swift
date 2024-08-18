6a4876399f0decec1b8f622041446f716649e9ed
Sithum Wickramanayaka <92084117+suwickramanayaka@users.noreply.github.com>
2024-08-19 00:04:46 +0530

    Initial commit for IOS Dice App
    This is the part of learning IOS development by creating a dice app

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,10 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // IBoutlet allows me to reference a UI element
+    @IBOutlet weak var diceImageView1: UIImageView!
+    
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // WHO         WHAT      VALUE
+        diceImageView1.image = UIImage(imageLiteralResourceName: "DiceSix")
+    
+        //diceImageView1.alpha = 0.5
+        
+        diceImageView2.image = UIImage(imageLiteralResourceName: "DiceTwo")
     }
 
 
